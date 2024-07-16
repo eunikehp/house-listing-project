d5465576ef81f97768a365cb046b7019ce4ae371
Eunike Pardede <eunikehedriani@gmail.com>
2024-07-16 14:24:05 +0200

    Edit vite config to allow env variables

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      vue(),
+    ],
+    define: {
+      VITE_API_BASE_URL: JSON.stringify(env.VITE_API_BASE_URL),
+      VITE_LISTING_API_KEY: JSON.stringify(env.VITE_LISTING_API_KEY)
+    }
+    ,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
