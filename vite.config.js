d7ac1f9eb3a081748417268e1a6bf8479f5f1e21
Eunike Pardede <eunikehedriani@gmail.com>
2024-07-16 16:27:58 +0200

    Edit env api url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [
